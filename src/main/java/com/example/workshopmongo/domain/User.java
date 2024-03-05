89c1818ccd8c393117bdb0cb5742dcf8c61d92a5
Thiago Tonelli Fabris <thifabris@gmail.com>
2024-03-05 12:08:20 -0300

    referenciados os posts do usuario

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -1,10 +1,12 @@
 package com.example.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //se deixar sem o collection o mongo puxa o nome da classe em minúsculo como nome da coleção
@@ -17,6 +19,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	
 	public User() {
@@ -53,6 +58,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -70,5 +83,4 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
